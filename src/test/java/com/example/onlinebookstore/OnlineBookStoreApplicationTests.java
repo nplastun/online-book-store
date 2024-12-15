d3f7c90757c6f30e91e298cd86d8335deb975080
Nataliya Plastun <natalya21753@gmail.com>
2024-12-15 14:22:46 +0200

    Add Book entity, repository, and service3

--- src/test/java/com/example/onlinebookstore/OnlineBookStoreApplicationTests.java
+++ src/test/java/com/example/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.onlinebookstore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = OnlineBookStoreApplication.class)
+@SpringBootTest(properties = {"spring.profiles.active=test"})
 public class OnlineBookStoreApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
 }
+
 
